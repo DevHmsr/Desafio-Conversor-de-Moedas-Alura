b2a2fc64017a9ff650b5c92e7435e73a117f3511
Heitor Matos <eng.heitormatos@gmail.com>
2025-05-12 12:02:39 -0300

    AdCorreção do bug de ExceÃsções

--- src/ApiTaxaConversao.java
+++ src/ApiTaxaConversao.java
@@ -2,12 +2,20 @@ import com.google.gson.Gson;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.OptionalDouble;
 
-public class ApiExchangeRate {
-    private static final String API_KEY = System.getenv("6728244f3d6232ef039e8154");
+public class ApiTaxaConversao {
+    private static final String API_KEY = "6728244f3d6232ef039e8154";
+
+    private static final Map<String, Double> cacheTaxas = new HashMap<>();
 
     public static OptionalDouble buscarTaxa(String de, String para) {
+        String chave = de + "->" + para;
+        if (cacheTaxas.containsKey(chave)) {
+            return OptionalDouble.of(cacheTaxas.get(chave));
+        }
         try {
             String urlString = String.format("https://v6.exchangerate-api.com/v6/%s/latest/%s", API_KEY, de);
             URL url = new URL(urlString);
--- src/ConversorMoedas.java
+++ src/ConversorMoedas.java
@@ -9,21 +9,29 @@ public class ConversorMoedas {
 
         while (true) {
             System.out.println("\n****************************************");
-            System.out.println("Seja bem-vindo(a) ao Conversor de Moeda!\n");
+            System.out.println("\nSeja bem-vindo(a) ao Conversor de Moedas!\n");
             System.out.println("Selecione uma opção:\n");
             System.out.println("1) Dólar => Peso argentino");
             System.out.println("2) Peso argentino => Dólar");
             System.out.println("3) Dólar => Real brasileiro");
             System.out.println("4) Real brasileiro => Dólar");
-            System.out.println("5) Dólar => Peso colombiano");
-            System.out.println("6) Peso colombiano => Dólar");
+            System.out.println("5) Dólar => Euro");
+            System.out.println("6) Euro => Dólar");
             System.out.println("7) Sair");
-            System.out.print("Escolha uma opção válida: ");
-            System.out.print("Escolha uma opção válida: ");
-            int opcao = scanner.nextInt();
+            System.out.print("\nEscolha uma opção válida: ");
+
+            int opcao;
+            if (scanner.hasNextInt()) {
+                opcao = scanner.nextInt();
+            } else {
+                System.out.println("\nVocê digitou uma opção inválida. Por favor, digite um número.");
+                scanner.next();
+                continue;
+            }
+
 
             if (opcao == 7) {
-                System.out.println("Saindo...");
+                System.out.println("\nSaindo...");
                 break;
             }
 
@@ -34,30 +42,42 @@ public class ConversorMoedas {
                 case 2 -> { de = "ARS"; para = "USD"; }
                 case 3 -> { de = "USD"; para = "BRL"; }
                 case 4 -> { de = "BRL"; para = "USD"; }
-                case 5 -> { de = "USD"; para = "COP"; }
-                case 6 -> { de = "COP"; para = "USD"; }
+                case 5 -> { de = "USD"; para = "EUR"; }
+                case 6 -> { de = "EUR"; para = "USD"; }
                 default -> {
-                    System.out.println("Opção inválida.");
-                    continue;  // Volta ao menu sem pedir valor
+                    System.out.println("\nOpção inválida.");
+                    continue;
                 }
             }
 
-            System.out.print("Digite o valor que deseja converter: ");
-            double valor = scanner.nextDouble();
-            if (valor <= 0) {
-                System.out.println("Valor inválido para conversão.");
-                continue;
+            double valor;
+            while (true) {
+                System.out.print("\nDigite o valor que deseja converter: ");
+                if (scanner.hasNextDouble()) {
+                    valor = scanner.nextDouble();
+                    if (valor <= 0) {
+                        System.out.println("\nValor inválido. Digite um número maior que zero.");
+                    } else {
+                        break;
+                    }
+                } else {
+                    System.out.println("\nEntrada inválida. Por favor, digite um número.");
+                    scanner.next();
+                }
             }
 
-            OptionalDouble taxaOpt = ApiExchangeRate.buscarTaxa(de, para);
+            OptionalDouble taxaOpt = ApiTaxaConversao.buscarTaxa(de, para);
             if (taxaOpt.isEmpty()) {
-                System.out.println("Erro ao obter a taxa de conversão.");
+                System.out.println("\nErro ao obter a taxa de conversão.");
             } else {
                 double taxa = taxaOpt.getAsDouble();
                 double convertido = valor * taxa;
+
+                System.out.printf("\nValor %.2f [%s] correspondente ao valor final de =>>> %.2f [%s]%n", valor, de,
+                        convertido, para);
+
                 CriarJson resultado = new CriarJson(valor, de, para, taxa, convertido);
                 String json = resultado.toJson();
-                System.out.println(json);
                 salvarJsonEmArquivo(json);
             }
         }
@@ -67,9 +87,9 @@ public class ConversorMoedas {
     private static void salvarJsonEmArquivo(String conteudoJson) {
         try (FileWriter writer = new FileWriter("conversoes.jsonl", true)) {
             writer.write(conteudoJson + System.lineSeparator());
-            System.out.println("Resultado salvo no arquivo " + "conversoes.jsonl");
+            System.out.println("\nResultado salvo no arquivo " + "conversoes.jsonl");
         } catch (IOException e) {
-            System.out.println("Erro ao salvar o arquivo: " + e.getMessage());
+            System.out.println("\nErro ao salvar o arquivo: " + e.getMessage());
         }
     }
 }

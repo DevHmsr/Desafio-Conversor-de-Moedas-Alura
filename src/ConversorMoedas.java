29890b21e1139e840d8eb4e87e71ed430af6a5cd
Heitor Matos <eng.heitormatos@gmail.com>
2025-05-13 19:02:38 -0300

    Adicao de currency codes

--- src/ConversorMoedas.java
+++ src/ConversorMoedas.java
@@ -1,3 +1,4 @@
+// ConversorMoedas.java
 import com.google.gson.Gson;
 
 import java.io.File;
@@ -13,18 +14,22 @@ public class ConversorMoedas {
 
         while (true) {
             System.out.println("\n********** Conversor de Moedas **********");
-            System.out.println("1) Dólar => Peso argentino");
-            System.out.println("2) Peso argentino => Dólar");
-            System.out.println("3) Dólar => Real brasileiro");
-            System.out.println("4) Real brasileiro => Dólar");
-            System.out.println("5) Dólar => Peso colombiano");
-            System.out.println("6) Peso colombiano => Dólar");
-            System.out.println("7) Ver histórico de conversões");
-            System.out.println("8) Sair");
+            System.out.println("1) Dólar => Peso Argentino");
+            System.out.println("2) Peso Argentino => Dólar");
+            System.out.println("3) Dólar => Real Brasileiro");
+            System.out.println("4) Real Brasileiro => Dólar");
+            System.out.println("5) Dólar => Peso Colombiano");
+            System.out.println("6) Peso Colombiano => Dólar");
+            System.out.println("7) Dólar => Boliviano");
+            System.out.println("8) Boliviano => Dólar");
+            System.out.println("9) Dólar => Peso Chileno");
+            System.out.println("10) Peso Chileno => Dólar");
+            System.out.println("11) Ver histórico de conversões");
+            System.out.println("12) Sair");
             System.out.print("\nEscolha uma opção: ");
 
-            int opcao = AssistenteInput.lerOpcao(scanner, 1, 8);
-            if (opcao == 8) {
+            int opcao = AssistenteInput.lerOpcao(scanner, 1, 12);
+            if (opcao == 12) {
                 System.out.println("Saindo...");
                 break;
             }
@@ -37,9 +42,14 @@ public class ConversorMoedas {
                 case 4 -> { de = "BRL"; para = "USD"; }
                 case 5 -> { de = "USD"; para = "COP"; }
                 case 6 -> { de = "COP"; para = "USD"; }
-                case 7 -> {
+                case 7 -> { de = "USD"; para = "BOB"; }
+                case 8 -> { de = "BOB"; para = "USD"; }
+                case 9 -> { de = "USD"; para = "CLP"; }
+                case 10 -> { de = "CLP"; para = "USD"; }
+                case 11 -> {
                     exibirHistoricoConversoes();
-                    continue;}
+                    continue;
+                }
             }
 
             double valor = AssistenteInput.lerDoublePositivo(scanner, "\nDigite o valor que deseja converter: ");
